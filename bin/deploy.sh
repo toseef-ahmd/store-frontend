11debc551733a52424ea933f5a72ac7a2e423a29
toseefdc360 <toseef@afnanilab.com>
2022-05-29 19:50:25 +0500

    added circle ci config

---
+++ bin/deploy.sh
@@ -0,0 +1,1 @@
+aws s3 cp --recursive --scl public-read ./dist s3://store-frontent/
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
   production: true,
+  BASE_URL: 'http://storebackendapi-env-1.eba-zfrc9crh.us-east-1.elasticbeanstalk.com'
 }
